88272b74cc1e01c0dbcf2216084d5ff7d0552a89
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-04-30 22:03:22 +0200

    fixed spelling

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:nba_stats_app/screens/splash_screen.dart';
 import 'package:nba_stats_app/themes/themes.dart';
+import 'package:nba_stats_app/widgets/bottom_nav_bar.dart';
 
 void main() {
 
