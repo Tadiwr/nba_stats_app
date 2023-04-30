bb855f5d532cb31d575e9b2a15630f90135d383e
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-04-30 20:17:50 +0200

    added onpresssed property to rounded button

--- lib/widgets/buttons.dart
+++ lib/widgets/buttons.dart
@@ -2,15 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:nba_stats_app/styles/text_styles.dart';
 
 class RoundedButton extends StatelessWidget {
-  const RoundedButton({super.key, required this.color, required this.text});
+  const RoundedButton({
+    super.key,
+    required this.color,
+    required this.text,
+    required this.onPressed
+  });
 
   final Color color;
   final String text;
+  final Function onPressed;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () => {},
+      onPressed: () => {
+        onPressed()
+      },
       style: ElevatedButton.styleFrom(
         backgroundColor: color,
         foregroundColor: Colors.black87,
