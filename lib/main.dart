8404f1e875c3b3d5b1b990a30a3f3081f7a87436
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-05-01 17:40:14 +0200

    Removed unused packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:nba_stats_app/screens/splash_screen.dart';
 import 'package:nba_stats_app/themes/themes.dart';
 import 'package:nba_stats_app/widgets/bottom_nav_bar.dart';
 
