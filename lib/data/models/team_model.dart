a9b85c2dfeb3af7b6e3d69c572b3c86375f4a537
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-05-01 21:35:13 +0200

    Refactored teamModel class

--- lib/data/models/team_model.dart
+++ lib/data/models/team_model.dart
@@ -1,27 +1,39 @@
-// ignore_for_file: prefer_typing_uninitialized_variables
-
 class TeamModel {
 
-  var id;
-  var displayName;
-  var color;
-  var athletes;
-  var location;
-  var name;
-  var abbreviation;
-  var alternativeColor;
-  var logoUrl;
+  final int id;
+  final String displayName;
+  final String color;
+  final String athletes;
+  final String location;
+  final String name;
+  final String abbreviation;
+  final String alternativeColor;
+  final String logoUrl;
 
-  TeamModel(Map<String, dynamic> json) {
-    id = json["id"];
-    displayName = json["displayName"];
-    color = json["color"];
-    athletes = json["athletes"];
-    location = json["location"];
-    name = json["name"];
-    abbreviation = json["abbreviation"];
-    logoUrl = json["logos"][0];
-    alternativeColor = json["alternativeColor"];
+  TeamModel({
+    required this.id,
+    required this.displayName,
+    required this.color,
+    required this.athletes, 
+    required this.location,
+    required this.name,
+    required this.abbreviation,
+    required this.logoUrl,
+    required this.alternativeColor
+  });
+
+  factory TeamModel.fromJson(Map<String, dynamic> json) {
+    return TeamModel(
+      id: json["id"],
+      displayName: json["displayName"],
+      color: json["color"],
+      athletes: json["athletes"],
+      location: json["location"],
+      name: json["name"],
+      abbreviation: json["abbreviation"],
+      logoUrl: json["logos"][0]["href"],
+      alternativeColor: json["alternativeColor"]
+    );
   }
 
 }
