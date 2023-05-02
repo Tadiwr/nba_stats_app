6147228d5fc4ced2c6eab1ac37985d7fb34f124f
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-05-02 12:18:16 +0200

    Added future builder that returns list of teams

--- lib/views/screens/team_screen.dart
+++ lib/views/screens/team_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:nba_stats_app/data/models/team_model.dart';
+import 'package:nba_stats_app/views/widgets/team_card.dart';
+import "package:nba_stats_app/data/apis/espn_core_api.dart";
 
 class TeamScreen extends StatefulWidget {
   const TeamScreen({super.key});
@@ -12,9 +13,35 @@ class _TeamScreenState extends State<TeamScreen> {
 
   @override
   Widget build(BuildContext context) {
+
+    EsonCoreApi().getTeams()
+    .then((json) => print(json));
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Teams")
+      ),
+      body: FutureBuilder(
+        future: EsonCoreApi().getTeams(),
+        builder: ((context, snapshot) {
+          if (snapshot.hasData) {
+            var list = snapshot.data;
+            return ListView.builder(
+              itemCount: list?.length,
+              itemBuilder: (context, index) {
+                return TeamCard(
+                  teamName: list?[index]["team"],
+                  teamAbrreviation: list?[index]["abbreviation"],
+                );
+              },
+            );
+          }
+
+          return const Center(
+            child: CircularProgressIndicator()
+          );
+        }),
+
       ),
     );
   }
