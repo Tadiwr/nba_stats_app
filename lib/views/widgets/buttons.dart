bdb573be2dc931375b4b5f52adf06eb89f9743bf
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-05-02 12:20:08 +0200

    removed color property from rounded button

--- lib/views/widgets/buttons.dart
+++ lib/views/widgets/buttons.dart
@@ -4,12 +4,10 @@ import 'package:nba_stats_app/styles/text_styles.dart';
 class RoundedButton extends StatelessWidget {
   const RoundedButton({
     super.key,
-    required this.color,
     required this.text,
     required this.onPressed
   });
 
-  final Color color;
   final String text;
   final Function onPressed;
 
@@ -20,7 +18,6 @@ class RoundedButton extends StatelessWidget {
         onPressed()
       },
       style: ElevatedButton.styleFrom(
-        backgroundColor: color,
         foregroundColor: Colors.black87,
         shape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(10))
