b56f7e56226d3b8ccc408ad52cc5aeca0ab70631
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-05-03 19:48:00 +0200

    Updated onboarding screen

--- lib/views/screens/splash_screen.dart
+++ lib/views/screens/splash_screen.dart
@@ -1,9 +1,12 @@
+import 'dart:async';
+import 'package:flutter_animator/flutter_animator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:nba_stats_app/styles/text_styles.dart';
-import 'package:nba_stats_app/views/widgets/bottom_nav_bar.dart';
+import 'package:nba_stats_app/views/screens/on_boarding_screen.dart';
 import 'package:nba_stats_app/views/widgets/buttons.dart';
 import 'dart:math' as math;
+import 'package:nba_stats_app/themes/colors.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -13,28 +16,52 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  Color green = const Color.fromARGB(255, 30, 255, 146);
 
-  int generateRandInt() {
-    final random = math.Random();
-    return random.nextInt(3) + 1;
+
+  int imageNumber = 1;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _timer = Timer.periodic(
+      const Duration(seconds: 5),
+      (timer) {
+        setState(() {
+          imageNumber = generateRandImageNum();
+        });
+      }
+    );
   }
 
-  Image loadSplashImage() {
-    final num = generateRandInt(); 
+  int generateRandImageNum() {
+      final random = math.Random();
+      return random.nextInt(10) + 1;
+  }
+
+  Image loadSplashImage(int num) {
     return Image(
-      image: AssetImage('assets/images/splash-${num}.jpg'),
+      image: AssetImage('assets/images/splash-$num.jpg'),
       width: double.infinity,
       height: double.infinity,
       fit: BoxFit.cover,
     );
   }
 
-  void navigateToHomeScreen() {
+  void navigateToOnBoarding() {
+
     Navigator.pushReplacement(
       context,
       MaterialPageRoute(
-        builder: (context) => const AppWraper()
+        builder: (context) => const OnBoardingScreen()
+      )
+    );
+
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        systemNavigationBarColor: appColors.surfaceGrey,
+        systemNavigationBarContrastEnforced: true,
       )
     );
   }
@@ -43,132 +70,101 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
 
     SystemChrome.setSystemUIOverlayStyle(
-      const SystemUiOverlayStyle(
-        systemNavigationBarColor: Colors.black
+      SystemUiOverlayStyle(
+        systemNavigationBarColor: appColors.green
       )
     );
 
     return Scaffold(
+      extendBodyBehindAppBar: true,
+      backgroundColor: appColors.green,
       appBar: AppBar(
-        elevation: 0,
-        centerTitle: true,
         title: const Text("Codapt"),
         backgroundColor: Colors.transparent,
       ),
-      extendBodyBehindAppBar: true,
+
+      // Stack of widgets
       body: Stack(
         alignment: Alignment.bottomCenter,
         children: [
-          Expanded(
-            flex: 1,
-            child: loadSplashImage(),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    // Colors.transparent,
-                    // Color.fromARGB(148, 0, 0, 0),
-                    // Color.fromARGB(150, 0, 0, 0),
-                    // Color.fromARGB(174, 0, 0, 0),
-                    // Color.fromARGB(183, 0, 0, 0),
-                    // Color.fromARGB(213, 0, 0, 0),
-                    // Color.fromARGB(222, 0, 0, 0),
-                    // Color.fromARGB(236, 0, 0, 0),
-                    // Color.fromARGB(239, 0, 0, 0),
-                    // Color.fromARGB(244, 0, 0, 0),
-                    // Colors.black,
-                    // Colors.black,
-                    // Colors.black,
-                    // Colors.black,
-                    // Colors.black,
-                    // Colors.black,
-                    Colors.transparent,
-                    Colors.black,
-                    Colors.black
-                  ],
-                  stops: [0.1, 0.5 ,1],
-                  begin: Alignment.center,
-                  end: Alignment.bottomCenter
-                )
-              ),
+
+          // image at the botttom
+          FadeIn(
+            child: SizedBox(
               width: double.infinity,
               height: double.infinity,
+              child: loadSplashImage(imageNumber),
             ),
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                width: double.infinity,
-                height: 400,
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [
-                      Colors.black,
-                      Colors.transparent,
-                    ],
-                    begin: Alignment.bottomCenter,
-                    end: Alignment.center
-                  )
-                ),
-              ),
-              Container(
-                color: Colors.black,
-                width: double.infinity,
-                height: 200
-              ),
-            ],
+
+          // gradient
+          Container(
+            width: double.infinity,
+            height: 500,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                colors: [
+                  appColors.green,
+                  Colors.transparent
+                ]
+              )
+            ),
           ),
-          Expanded(
-            flex: 1,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  width: double.infinity,
-                  child: Text(
-                    "NBA STATISTICS",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 40,
+
+          FadeInUp(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: SizedBox(
+                width: double.infinity,
+                height: 500,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "NBA Statistics For Every Fan",
+                      softWrap: true,
+                      maxLines: 3,
+                      style: h1.copyWith(
+                        fontSize: 55
+                      ),
                     ),
-                    softWrap: true,
-                  )
-                ),
-                const SizedBox(
-                  width: double.infinity,
-                  child: Text(
-                    "FOR EVERY FAN!",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.w900,
-                    ),
-                    maxLines: 2,
-                    softWrap: true
-                  )
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Container(
-                    width: double.infinity,
-                    height: 60,
-                    child: RoundedButton(
-                      text: "Take Me There",
-                      onPressed: () {navigateToHomeScreen();},
-                    ),
-                  ),
-                )
-              ],
+            
+                    FadeInLeft(
+                      child: RoundedButton(
+                        text: "Take Me There",
+                        color: appColors.darkGrey,
+                        foregroundColor: Colors.white,
+                        onPressed: () {
+                          navigateToOnBoarding();
+                        },
+                      ),
+                    )
+            
+                  ],
+                ),  
+              ),
             ),
           )
+          // text and button
         ],
       ),
     );
+
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        systemNavigationBarColor: appColors.surfaceGrey,
+        systemNavigationBarContrastEnforced: true,
+      )
+    );
+
+    super.dispose();
+    _timer.cancel();
   }
 }
