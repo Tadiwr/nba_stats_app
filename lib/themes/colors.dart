a851908886ad170c97469b3429e3a7acb06491c4
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-05-03 19:47:14 +0200

    created colors class

---
+++ lib/themes/colors.dart
@@ -0,0 +1,19 @@
+library app_colors;
+import 'dart:ui';
+
+export 'colors.dart';
+
+/// MyColors class defines all the colors that will are 
+/// globally used across the app
+
+class MyColors {
+  final Color green = const Color.fromARGB(255, 35, 250, 42);
+  final Color surfaceGrey = const Color.fromARGB(255, 16, 16, 16);
+  final Color darkGrey =  const Color.fromARGB(255, 16, 16, 16);
+  final Color lightGrey = const Color.fromARGB(255, 58, 58, 58);
+}
+
+///  This instance is ment to be accessed by the rest of the app 
+/// avoid having to instatiate the the MyColors app over and over again
+
+final MyColors appColors = MyColors();
