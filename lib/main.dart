d654bb43eeb390d4776babc43a624f739d17bc6c
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-05-03 19:47:04 +0200

    Improved theme of app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:nba_stats_app/themes/colors.dart';
 import 'package:nba_stats_app/views/screens/splash_screen.dart';
 
 void main() {
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-      systemNavigationBarColor: Color.fromARGB(255, 16, 16, 16),
+
+  // Sets the color of the navigation bar on android
+  SystemChrome.setSystemUIOverlayStyle( SystemUiOverlayStyle(
+      systemNavigationBarColor: appColors.surfaceGrey,
       statusBarIconBrightness: Brightness.light,
-      statusBarBrightness: Brightness.light));
+      statusBarBrightness: Brightness.light
+  ));
+
   runApp(const MainApp());
 }
 
@@ -16,30 +21,34 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Nba Stats",
+      darkTheme: darkTheme,
       themeMode: ThemeMode.dark,
       debugShowCheckedModeBanner: false,
+
+      // TODO: Add logic to allow the app to descide whether to navigate to
+      // splash screen or home page!
       home: const SplashScreen(),
-      darkTheme: ThemeData(
-        fontFamily: 'Helvatica',
-        scaffoldBackgroundColor: const Color.fromARGB(255, 16, 16, 16),
-        colorScheme: const ColorScheme.dark().copyWith(
-          primary: const Color.fromARGB(255, 35, 250, 42),
-          secondary: const Color.fromARGB(255, 71, 71, 71),
-          surface: const Color.fromARGB(255, 16, 16, 16),
-          background: const Color.fromARGB(255, 16, 16, 16),
-        ),
-        appBarTheme: const AppBarTheme(
-            titleTextStyle: TextStyle(
-              fontWeight: FontWeight.bold, 
-              fontSize: 25,
-            ),
-            systemOverlayStyle:
-                SystemUiOverlayStyle(statusBarColor: Colors.transparent),
-            centerTitle: true,
-            elevation: 0
-          ),
-      ),
-      title: "Nba Stats",
     );
   }
 }
+
+final darkTheme = ThemeData(
+  fontFamily: 'Helvatica',
+  scaffoldBackgroundColor: appColors.surfaceGrey,
+  colorScheme: const ColorScheme.dark().copyWith(
+    primary: appColors.green,
+    secondary: appColors.lightGrey,
+    surface: appColors.surfaceGrey,
+    background: appColors.darkGrey
+  ),
+  appBarTheme: const AppBarTheme(
+      centerTitle: true,
+      elevation: 0,
+      systemOverlayStyle: SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+      titleTextStyle: TextStyle(
+        fontWeight: FontWeight.bold, 
+        fontSize: 25,
+      ),
+    ),
+);
