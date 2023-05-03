98832bd96f0ad43c0494c8236851b7338b555331
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-05-03 19:49:46 +0200

    improved team screen

--- lib/views/screens/team_screen.dart
+++ lib/views/screens/team_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:nba_stats_app/views/screens/team_details_screen.dart';
 import 'package:nba_stats_app/views/widgets/team_card.dart';
 import "package:nba_stats_app/data/apis/espn_core_api.dart";
+
+import '../../themes/colors.dart';
 
 class TeamScreen extends StatefulWidget {
   const TeamScreen({super.key});
@@ -13,6 +17,14 @@ class _TeamScreenState extends State<TeamScreen> {
 
   @override
   Widget build(BuildContext context) {
+
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        systemNavigationBarColor: appColors.surfaceGrey,
+        systemNavigationBarContrastEnforced: true,
+      )
+    );
+
 
     return Scaffold(
       appBar: AppBar(
@@ -30,6 +42,16 @@ class _TeamScreenState extends State<TeamScreen> {
                   teamName: list?[index]["team"],
                   teamAbrreviation: list?[index]["abbreviation"],
                   teamId: list?[index]["id"],
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => TeamDetailsScreen(
+                          teamId: list?[index]["id"],
+                        ) 
+                      )
+                    );
+                  },
                 );
               },
             );
