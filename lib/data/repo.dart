b06dc17fde15af4f9e1e0d114026021b49d5421e
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-06-18 01:22:16 +0200

    Created repo file

---
+++ lib/data/repo.dart
@@ -0,0 +1,21 @@
+import 'package:nba_stats_app/data/apis/espn_core_api.dart';
+import 'package:nba_stats_app/data/models/scoreboard_model.dart';
+import 'package:nba_stats_app/data/models/team_model.dart';
+
+/// Exposes data from different data sources
+class Repo {
+  final _espn = EsonCoreApi();
+
+  Future<TeamModel> getTeamData(int teamId) {
+    return _espn.getSingleTeamData(teamId);
+  }
+
+  Future<List<dynamic>> getTeams() async{
+    return _espn.getTeams();
+  } 
+
+  Future<ScoreBoardModel> getScoreBoard() {
+    return _espn.getScoreBoard();
+  }
+
+}
